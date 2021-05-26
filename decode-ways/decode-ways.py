4cd8d4c1c37e5f6c08c6b86c5cf49eaff64b74f8
Janani Sankarasubramanian <52078565+Jananishankar11@users.noreply.github.com>
2021-05-26 14:40:57 -0400

    Time: 32 ms (64.25%), Space: 14.3 MB (38.22%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -5,15 +5,29 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        length = len(s)
-        if length==0:
-            return 0
-        dp = [0 for _ in range(length+1)]
-        dp[length] = 1
-        dp[length-1] = 1 if s[length-1]!='0' else 0
-        for i in range(length-2, -1, -1):
-            if s[i] == '0':
-                continue
-            else:
-                dp[i] = dp[i+1]+dp[i+2] if (int(s[i:i+2])<=26) else dp[i+1]
-        return dp[0]
+        previous_number_of_ways=0
+        number_of_ways=int(s>'')
+        previous_digit=''
+    
+        for digit in s:
+            copy_previous=previous_number_of_ways
+            previous_number_of_ways=number_of_ways
+
+        #check if the current digit is greater than zero
+        #If it is, then (digit>'0') becomes true, and we multiply that by the total number of ways so far
+        #...Think permutations
+            number_of_ways=(digit>'0')*number_of_ways
+        
+        #At the same time, we check if the current digit and the next digit, are less than 27..
+        #If they are, then its another permutation possibility
+        #Again, we are checking the truth, and multiplying it by the number of previously known was, not
+        #the current number of ways
+        #
+        #We have the 9< check because if the digit we are on is a zero, then we won't count the 
+        #next permutation
+            number_of_ways+= (9<int(previous_digit+digit)<27)*copy_previous
+
+        #Finally, remember that our last digit was the digit we are on now
+            previous_digit=digit
+        
+        return number_of_ways
