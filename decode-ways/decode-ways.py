22022625a704ea4da049e14fbd5cdd3c52bc691e
Janani Sankarasubramanian <52078565+Jananishankar11@users.noreply.github.com>
2021-05-25 21:17:32 -0400

    Time: 28 ms (86.90%), Space: 14.1 MB (85.09%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -5,19 +5,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if s[0] == "0" or s is None or len(s)==0:
+        length = len(s)
+        if length==0:
             return 0
-    
-        dp = [0 for _ in range(len(s)+1)]
-        dp[0] = 1
-        dp[1] = 1 if s[0]!='0' else 0
-        for i in range(2, len(s)+1):
-            first = int(s[i-1:i])
-            second = int(s[i-2:i])
-            if first>=1 and first<=9:
-                dp[i] += dp[i-1]
-            
-            if second>=10 and second<=26:
-                dp[i] += dp[i-2]
-                
-        return dp[len(s)]
+        dp = [0 for _ in range(length+1)]
+        dp[length] = 1
+        dp[length-1] = 1 if s[length-1]!='0' else 0
+        for i in range(length-2, -1, -1):
+            if s[i] == '0':
+                continue
+            else:
+                dp[i] = dp[i+1]+dp[i+2] if (int(s[i:i+2])<=26) else dp[i+1]
+        return dp[0]
