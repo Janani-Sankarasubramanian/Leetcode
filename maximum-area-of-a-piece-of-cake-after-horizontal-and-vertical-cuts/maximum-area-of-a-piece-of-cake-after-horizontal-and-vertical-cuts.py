f4dd21f103e0673aff28a730840678f76c3bb142
Janani Sankarasubramanian <52078565+Jananishankar11@users.noreply.github.com>
2021-05-27 16:46:35 -0400

    Time: 296 ms (85.76%), Space: 27 MB (93.02%) - LeetHub

--- maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -1,24 +1,7 @@
 class Solution:
     def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
-        maxi_width,maxi_height  = 0,0
-        verticalCuts.sort()
         horizontalCuts.sort()
-        if horizontalCuts[0] <= h - horizontalCuts[-1]:
-            maxi_height = h - horizontalCuts[-1]
-        else:
-            maxi_height = horizontalCuts[0] 
-
-        for i in range(1, len(horizontalCuts)):
-            if maxi_height <= horizontalCuts[i]-horizontalCuts[i-1]:
-                maxi_height = horizontalCuts[i]-horizontalCuts[i-1]
-                
-        if verticalCuts[0] <= w - verticalCuts[-1]:
-            maxi_width = w - verticalCuts[-1]
-        else:
-            maxi_width = verticalCuts[0]
-            
-        for i in range(1, len(verticalCuts)):
-            if maxi_width <= verticalCuts[i]-verticalCuts[i-1]:
-                maxi_width = verticalCuts[i]-verticalCuts[i-1]
-                
-        return (maxi_height*maxi_width) % (10**9 + 7)
+        verticalCuts.sort()
+        h = max(map(lambda a: a[0] - a[1], zip(horizontalCuts + [h], [0] + horizontalCuts)))
+        w = max(map(lambda a: a[0] - a[1], zip(verticalCuts + [w], [0] + verticalCuts)))
+        return h * w % 1000000007
