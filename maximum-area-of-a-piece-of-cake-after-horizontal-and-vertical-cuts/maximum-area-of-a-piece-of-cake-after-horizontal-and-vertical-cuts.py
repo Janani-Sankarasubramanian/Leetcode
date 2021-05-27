fb24a7400b997192774090c6ad904d153092b33f
Janani Sankarasubramanian <52078565+Jananishankar11@users.noreply.github.com>
2021-05-27 12:11:24 -0400

    Time: 296 ms (85.70%), Space: 27.1 MB (76.99%) - LeetHub

--- maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -1,13 +1,24 @@
 class Solution:
     def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
-        maxi_width = 0
-        maxi_height = 0
-        verticalCuts = sorted(verticalCuts)
-        horizontalCuts = sorted(horizontalCuts)
-        maxi_height = max(horizontalCuts[0], h - horizontalCuts[-1])
+        maxi_width,maxi_height  = 0,0
+        verticalCuts.sort()
+        horizontalCuts.sort()
+        if horizontalCuts[0] <= h - horizontalCuts[-1]:
+            maxi_height = h - horizontalCuts[-1]
+        else:
+            maxi_height = horizontalCuts[0] 
+
         for i in range(1, len(horizontalCuts)):
-            maxi_height = max(maxi_height, horizontalCuts[i]-horizontalCuts[i-1])
-        maxi_width = max(verticalCuts[0], w - verticalCuts[-1])
+            if maxi_height <= horizontalCuts[i]-horizontalCuts[i-1]:
+                maxi_height = horizontalCuts[i]-horizontalCuts[i-1]
+                
+        if verticalCuts[0] <= w - verticalCuts[-1]:
+            maxi_width = w - verticalCuts[-1]
+        else:
+            maxi_width = verticalCuts[0]
+            
         for i in range(1, len(verticalCuts)):
-            maxi_width = max(maxi_width, verticalCuts[i]-verticalCuts[i-1])
+            if maxi_width <= verticalCuts[i]-verticalCuts[i-1]:
+                maxi_width = verticalCuts[i]-verticalCuts[i-1]
+                
         return (maxi_height*maxi_width) % (10**9 + 7)
