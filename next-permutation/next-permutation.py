1ae1e3cabd2c3f01d9fc237a6e494d5486725c6f
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2021-06-13 13:32:48 -0400

    Time: 44 ms (53.41%), Space: 14.3 MB (51.96%) - LeetHub

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -3,22 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def reverse(nums, start):
-            i = start
-            j = len(nums) - 1
-            while(i<j):
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-                j -= 1
-
-
-        i = len(nums) - 2
-        while (i>=0 and nums[i+1]<=nums[i]):
-            i -=1
-        if i>=0:
-            j = len(nums) -1
-            while nums[j] <= nums[i]:
-                j -= 1
-            nums[i], nums[j] = nums[j], nums[i]
-
-        reverse(nums,i+1)
+        i = j = len(nums)-1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+        if i == 0:   # nums are in descending order
+            nums.reverse()
+            return 
+        k = i - 1    # find the last "ascending" position
+        while nums[j] <= nums[k]:
+            j -= 1
+        nums[k], nums[j] = nums[j], nums[k]  
+        l, r = k+1, len(nums)-1  # reverse the second part
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l +=1 ; r -= 1
