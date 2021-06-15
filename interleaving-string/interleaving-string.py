359eddabd88cddf937c5e07646acac5946221932
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2021-06-15 19:04:06 -0400

    Time: 40 ms (70.04%), Space: 14.6 MB (37.30%) - LeetHub

--- interleaving-string/interleaving-string.py
+++ interleaving-string/interleaving-string.py
@@ -1,15 +1,21 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        r, c, l= len(s1), len(s2), len(s3)
-        if r+c != l:
+        def is_interleave(s1,i, s2,j, s3,k,memo):
+            if i == len(s1):
+                return s2[j:] == s3[k:]
+            if j == len(s2):
+                return s1[i:] == s3[k:]
+            if memo[i][j] >=0:
+                return True if memo[i][j]==1 else False
+            ans = False
+            if (s3[k] == s1[i] and is_interleave(s1,i+1,s2,j,s3,k+1,memo)
+                or s3[k]==s2[j] and is_interleave(s1,i,s2,j+1, s3, k+1,memo)):
+                ans = True
+            memo[i][j] = 1 if ans else 0
+            return ans
+
+    
+        if len(s1) + len(s2) != len(s3):
             return False
-        queue, visited = [(0, 0)], set((0, 0))
-        while queue:
-            x, y = queue.pop(0)
-            if x+y == l:
-                return True
-            if x+1 <= r and s1[x] == s3[x+y] and (x+1, y) not in visited:
-                queue.append((x+1, y)); visited.add((x+1, y))
-            if y+1 <= c and s2[y] == s3[x+y] and (x, y+1) not in visited:
-                queue.append((x, y+1)); visited.add((x, y+1))
-        return False
+        memo = [[-1 for _ in range(len(s2))] for _ in range(len(s1))]
+        return is_interleave(s1,0,s2,0,s3,0,memo)
