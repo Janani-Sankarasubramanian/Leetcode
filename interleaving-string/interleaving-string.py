f49bf4aeef1d1a1aab112732896db1ffb581ab51
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2021-06-16 06:28:49 -0400

    Time: 44 ms (60.29%), Space: 14.3 MB (77.65%) - LeetHub

--- interleaving-string/interleaving-string.py
+++ interleaving-string/interleaving-string.py
@@ -3,17 +3,17 @@ class Solution:
         if len(s1) + len(s2) != len(s3):
             return False
 
-        dp = [[False for j in range(len(s2)+1)] for i in range(len(s1)+1)]
+        dp = [False for j in range(len(s2)+1)]
 
         for i in range(len(s1)+1):
             for j in range(len(s2)+1):
                 if (i==0) and (j==0):
-                    dp[i][j] = True
+                    dp[j] = True
                 elif (i==0):
-                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[i+j-1]
+                    dp[j] = dp[j-1] and s2[j-1] == s3[i+j-1]
                 elif (j==0):
-                    dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i+j-1]
+                    dp[j] = dp[j] and s1[i-1] == s3[i+j-1]
                 else:
-                    dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+                    dp[j] = (dp[j] and s1[i-1] == s3[i+j-1]) or (dp[j-1] and s2[j-1] == s3[i+j-1])
 
-        return dp[len(s1)][len(s2)]
+        return dp[len(s2)]
