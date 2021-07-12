d3fd5554b665f29202537c5775f316df229f775c
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2021-07-12 17:43:43 -0400

    Time: 64 ms (10.10%), Space: 14.3 MB (31.03%) - LeetHub

--- check-array-formation-through-concatenation/check-array-formation-through-concatenation.py
+++ check-array-formation-through-concatenation/check-array-formation-through-concatenation.py
@@ -1,10 +1,10 @@
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
-        mp = {x[0]: x for x in pieces}
-        res = []
-        
-        for num in arr:
-            res += mp.get(num, [])
-            
-        return res == arr
+        keys, ans = {}, []
+        for piece in pieces:
+            keys[piece[0]] = piece
+        for a in arr:
+            if a in keys:
+                ans.extend(keys[a])
+        return ''.join(map(str, arr)) == ''.join(map(str, ans))
             
