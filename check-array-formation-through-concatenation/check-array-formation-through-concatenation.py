7b78733613e3e2ee764f2721bc80d08e3305d4d7
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2021-07-12 10:37:50 -0400

    Time: 52 ms (18.59%), Space: 14.4 MB (31.47%) - LeetHub

--- check-array-formation-through-concatenation/check-array-formation-through-concatenation.py
+++ check-array-formation-through-concatenation/check-array-formation-through-concatenation.py
@@ -1,19 +1,32 @@
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
         n = len(arr)
+        p_len = len(pieces)
+        pieces.sort()
+
         i = 0
         while i < n:
-            # find target piece
-            for p in pieces:
-                if p[0] == arr[i]:
+            left = 0
+            right = p_len - 1
+            found = -1
+            # use binary search to find target piece:
+            while left <= right:
+                mid = (left + right)//2
+                if pieces[mid][0] == arr[i]:
+                    found = mid
                     break
-            else:
+                elif pieces[mid][0] > arr[i]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            if found == -1:
                 return False
             # check target piece
-            # python saves the last iterated `p`
-            for x in p:
+            target_piece = pieces[found]
+            for x in target_piece:
                 if x != arr[i]:
                     return False
                 i += 1
+
         return True
             
