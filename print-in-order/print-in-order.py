3c0252cb5ba1b9f746b7835db8cc0e2756488216
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2021-11-10 00:12:58 -0500

    Time: 40 ms (72.54%), Space: 14.8 MB (8.17%) - LeetHub

--- print-in-order/print-in-order.py
+++ print-in-order/print-in-order.py
@@ -1,29 +1,19 @@
-from threading import Lock
+from threading import Barrier
+
 class Foo:
     def __init__(self):
-        self.firstJobDone = Lock()
-        self.secondJobDone = Lock()
-        self.firstJobDone.acquire()
-        self.secondJobDone.acquire()
-
-
-    def first(self, printFirst: 'Callable[[], None]') -> None:
-       # printFirst() outputs "first". Do not change or remove this line.
+        self.first_barrier = Barrier(2)
+        self.second_barrier = Barrier(2)
+            
+    def first(self, printFirst):
         printFirst()
-        self.firstJobDone.release()
-
-
-    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self.first_barrier.wait()
         
-        with self.firstJobDone:
-            # printSecond() outputs "second". Do not change or remove this line.
-            printSecond()
-            self.secondJobDone.release()
-        
-
-
-    def third(self, printThird: 'Callable[[], None]') -> None:
-       
-        with self.secondJobDone:
-            # printThird() outputs "third". Do not change or remove this line.
-            printThird()
+    def second(self, printSecond):
+        self.first_barrier.wait()
+        printSecond()
+        self.second_barrier.wait()
+            
+    def third(self, printThird):
+        self.second_barrier.wait()
+        printThird()
