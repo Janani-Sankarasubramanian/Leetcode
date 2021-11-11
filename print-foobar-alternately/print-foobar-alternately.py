6dcf004db18af158f0a7c390da2d3e90d5f91472
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2021-11-11 13:43:44 -0500

    Time: 72 ms (30.16%), Space: 14.9 MB (16.85%) - LeetHub

--- print-foobar-alternately/print-foobar-alternately.py
+++ print-foobar-alternately/print-foobar-alternately.py
@@ -1,24 +1,22 @@
-from threading import Condition
+from threading import Event
 
 class FooBar:
     def __init__(self, n):
         self.n = n
-        self.foo_counter = 0
-        self.bar_counter = 0
-        self.condition = Condition()
+        self.foo_printed = Event()
+        self.bar_printed = Event()
+        self.bar_printed.set()
 
     def foo(self, printFoo):
         for i in range(self.n):
-            with self.condition:
-                self.condition.wait_for(lambda: self.foo_counter == self.bar_counter)
-                printFoo()
-                self.foo_counter += 1
-                self.condition.notify(1)
+            self.bar_printed.wait()
+            self.bar_printed.clear()
+            printFoo()
+            self.foo_printed.set()
 
     def bar(self, printBar):
         for i in range(self.n):
-            with self.condition:
-                self.condition.wait_for(lambda: self.foo_counter > self.bar_counter)
-                printBar()
-                self.bar_counter += 1
-                self.condition.notify(1)            
+            self.foo_printed.wait()
+            self.foo_printed.clear()
+            printBar()
+            self.bar_printed.set()
