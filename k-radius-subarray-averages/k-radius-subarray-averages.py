1d5144d447761ba670a7d0270ac50d049ac47345
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2021-12-29 16:19:16 -0500

    Time: 1704 ms (81.23%), Space: 33.2 MB (26.66%) - LeetHub

---
+++ k-radius-subarray-averages/k-radius-subarray-averages.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        res = [-1]*len(nums)
+
+        left, curWindowSum, radius = 0, 0, 2*k+1
+        for right in range(len(nums)):
+            curWindowSum += nums[right]
+            if (right-left+1 >= radius):
+                res[left+k] = curWindowSum//radius
+                curWindowSum -= nums[left]
+                left += 1
+        return res
