c8ee7618526e02b2dd8a1c8d2cfa5e7bfa07b85c
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-01-12 11:23:20 -0500

    Time: 601 ms (44.50%), Space: 16.8 MB (71.24%) - LeetHub

--- 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.py
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.py
@@ -1,16 +1,20 @@
 class Solution:
     def subarraysWithKDistinct(self, A: List[int], K: int) -> int:
-        return self.atMostK(A, K) - self.atMostK(A, K - 1)
-
-    def atMostK(self, A, K):
-        count = collections.Counter()
-        res = i = 0
-        for j in range(len(A)):
-            if count[A[j]] == 0: K -= 1
-            count[A[j]] += 1
-            while K < 0:
-                count[A[i]] -= 1
-                if count[A[i]] == 0: K += 1
-                i += 1
-            res += j - i + 1
+        freq = {} #Map
+        left_1 = 0 #First left pointer
+        left_2 = 0 #second left pointer
+        res = 0 #result
+        for i, x in enumerate(A):
+            freq[x] = freq.get(x, 0) + 1 #update the map with the count
+            if len(freq) == K + 1:
+                # remove the distinct from left_2, move left_2, left_1
+                del freq[A[left_2]]
+                left_2 += 1
+                left_1 = left_2
+            if len(freq) == K:
+                # update left_2 and res (Notice: K >= 1)
+                while freq[A[left_2]] > 1:
+                    freq[A[left_2]] -= 1
+                    left_2 += 1
+                res += left_2 - left_1 +1
         return res
