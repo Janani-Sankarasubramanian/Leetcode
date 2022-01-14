1b67fbbd8536e7cdc80634d218f79bd1e851a2bc
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-01-14 08:25:06 -0500

    Time: 34 ms (77.85%), Space: 14.1 MB (80.84%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
@@ -1,24 +1,12 @@
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
-        # k and i are two pointers in typical same direction sliding window problem, j is used to mark start point
-        j, k, vow, res = 0, 0, 0, 0
-        maps = {"a": 0, "e": 0, "i": 0, "o": 0, "u": 0}
-        for i in range(len(word)):
-            if word[i] in maps:
-                maps[word[i]] += 1
-                if maps[word[i]] == 1:
-                    vow += 1
-                while vow == 5:
-                    maps[word[k]] -= 1
-                    if maps[word[k]] == 0:
-                        vow -= 1
-                    k += 1
-                res += k-j
+        ans, last_consonant = 0, -1
+        last_seen_vowels = {v: -1 for v in "aeiou"}
+        for i, x in enumerate(word):
+            if x in last_seen_vowels:
+                last_seen_vowels[x] = i
+                ans += max(min(last_seen_vowels.values()) - last_consonant, 0 )
             else:
-                # reset pointers and maps
-                maps = {"a": 0, "e": 0, "i": 0, "o": 0, "u": 0}
-                vow = 0
-                j, k = i+1, i+1
-        return res
-                
+                last_consonant = i
+        return ans
             
