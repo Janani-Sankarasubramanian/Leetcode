661ca7a1e7ef24646f6308e2bcf14e6421f1a7cd
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-01-31 13:37:46 -0500

    Time: 36 ms (60.34%), Space: 14.1 MB (94.77%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,19 +1,12 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         LEFT_BOUND, RIGHT_BOUND = len(s), len(t)
+        
+        p_left = p_right = 0
+        while p_left < LEFT_BOUND and p_right < RIGHT_BOUND:
+            # move both pointers or just the right pointer
+            if s[p_left] == t[p_right]:
+                p_left += 1
+            p_right += 1
 
-        def rec_isSubsequence(left_index, right_index):
-            # base cases
-            if left_index == LEFT_BOUND:
-                return True
-            if right_index == RIGHT_BOUND:
-                return False
-            # consume both strings or just the target string
-            if s[left_index] == t[right_index]:
-                left_index += 1
-            right_index += 1
-
-            return rec_isSubsequence(left_index, right_index)
-
-        return rec_isSubsequence(0, 0)
-    
+        return p_left == LEFT_BOUND
