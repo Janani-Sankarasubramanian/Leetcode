d0f071755772872c914afa15a040f88bec2fa088
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-02-12 22:52:34 -0500

    Time: 519 ms (17.63%), Space: 17.8 MB (36.94%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -4,12 +4,12 @@ class Solution:
         for i in range(len(word1)+1):
             for j in range(len(word2)+1):
                 if i ==0 or j ==0:
-                    continue
-                if word1[i-1] == word2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    dp[i][j] = i+j
+                elif word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        return len(word1) + len(word2) - 2*dp[len(word1)][len(word2)]
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1])
+        return dp[len(word1)][len(word2)]
     
 
         
