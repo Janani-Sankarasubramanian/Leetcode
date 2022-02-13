247f317c3ee9c57c0c33dae800f0eb07410f443f
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-02-12 20:49:53 -0500

    Time: 414 ms (36.73%), Space: 16.1 MB (78.65%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -1,18 +1,15 @@
 class Solution:
-    def lcs(self, s1, s2, m, n, memo):
-        if m ==0 or n==0:
-            return 0 
-        if memo[m][n] >0 :
-            return memo[m][n]
-        if s1[m-1] == s2[n-1]:
-            memo[m][n] = 1 + self.lcs(s1, s2, m-1, n-1, memo)
-        else:
-            memo[m][n] = max(self.lcs(s1, s2, m, n-1, memo), self.lcs(s1,s2, m-1,n, memo))
-        return memo[m][n]
-        
     def minDistance(self, word1: str, word2: str) -> int:
-        memo = [[0 for i in range(len(word2)+1)] for j in range(len(word1)+1)]
-        return len(word1) + len(word2) - 2*self.lcs(word1, word2, len(word1), len(word2), memo)
+        dp = [[0 for i in range(len(word2)+1)] for j in range(len(word1)+1)]
+        for i in range(len(word1)+1):
+            for j in range(len(word2)+1):
+                if i ==0 or j ==0:
+                    continue
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return len(word1) + len(word2) - 2*dp[len(word1)][len(word2)]
     
 
         
