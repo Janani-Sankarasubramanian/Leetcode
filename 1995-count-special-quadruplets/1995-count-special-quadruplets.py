fb0ee7b287dcad28f8d58f3b12cc66748b9094a4
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-02-22 10:28:53 -0500

    Time: 83 ms (89.87%), Space: 14 MB (63.77%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.py
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.py
@@ -1,13 +1,11 @@
 class Solution:
     def countQuadruplets(self, nums: List[int]) -> int:
-        seen = defaultdict(int)
-        n = len(nums)
         count = 0
-        for a in range(n-1, -1, -1):
+        n = len(nums)
+        seen = defaultdict(int)
+        for a in range(n-1, 0, -1):
             for b in range(a-1, -1, -1):
-                for c in range(b-1, -1, -1):
-                    maybe_d = nums[a] + nums[b] + nums[c]
-                    if maybe_d in seen:
-                        count += seen[maybe_d]
-            seen[nums[a]] += 1
+                count += seen[nums[a] +nums[b]]
+            for d in range(n-1, a, -1):
+                seen[nums[d] - nums[a]] += 1
         return count
