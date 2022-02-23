a8ba655c7170cc0ed48db66b829436389039be1e
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-02-22 21:29:12 -0500

    Time: 1296 ms (26.20%), Space: 16.6 MB (5.65%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.py
+++ 454-4sum-ii/454-4sum-ii.py
@@ -1,14 +1,26 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        cnt = 0
         m = defaultdict(int)
-        for a in nums1:
-            for b in nums2:
-                m[a+b] += 1
+        lists = [nums1, nums2, nums3, nums4]
         
-        for c in nums3:
-            for d in nums4:
-                cnt += m[-(c+d)]
+        def nSumCount() -> int:
+            addToHash(0,0)
+            return countComplements(len(lists) // 2, 0)
         
-        return cnt
+        def addToHash(i: int, total: int) -> None:
+            if i == len(lists) // 2:
+                m[total] = m[total] + 1
+            else:
+                for a in lists[i]:
+                    addToHash(i+1, total+a)
+        
+        def countComplements(i: int, complement: int) -> int:
+            if i == len(lists):
+                return m[complement]
+            cnt = 0
+            for a in lists[i]:
+                cnt += countComplements(i+1, complement -a)
+            return cnt
+        
+        return nSumCount()
             
