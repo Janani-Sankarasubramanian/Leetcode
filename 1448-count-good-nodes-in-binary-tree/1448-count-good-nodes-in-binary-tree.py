00bed1e27ffb0bc6ba7c49addda2f3b5d455da42
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-07-04 12:15:05 -0400

    Time: 483 ms (19.07%), Space: 32.7 MB (7.94%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -1,4 +1,5 @@
 # Definition for a binary tree node.
+from collections import deque 
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
@@ -6,30 +7,25 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        #Start by creating a stack and append the root node and the max value to it, use a Tuple may be
-        #Pop the last element out of the list and compare the max value and the root node
+        #Start by creating a queue and append the root node and the max value to it, use a Tuple may be
+        #Pop the first element out of the list and compare the max value and the root node
         #if the comparison is greater, increment the num of good nodes
         #Add the left and right children of the node to the stack, along with its max value so far
         #Iterate until the stack is empty and return the final num of good nodes
-        
-        stack = [(root, root.val)]
-        
+                    
+        queue = collections.deque([(root, float(-inf))])
         num_good_nodes = 0
         
-        while stack:
-            node, max_so_far = stack.pop()
+        while queue:
+            node, max_so_far = queue.popleft()
             
-            if node.val >= max_so_far:
+            if max_so_far <= node.val:
                 num_good_nodes += 1
-                max_so_far = node.val
-            
-            if node.left:
-                stack.append((node.left, max_so_far))
             
             if node.right:
-                stack.append((node.right, max_so_far))
-        
-        return num_good_nodes
+                queue.append((node.right, max(node.val, max_so_far)))
                 
-            
-        
+            if node.left:            
+                queue.append((node.left, max(node.val, max_so_far)))
+                
+        return num_good_nodes
