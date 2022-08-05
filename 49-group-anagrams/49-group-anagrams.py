b7c5db5ff87ce6d23b851b589cad55614a81f607
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-08-04 21:57:11 -0400

    Time: 157 ms (55.85%), Space: 18.5 MB (41.69%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,11 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        Anagram_map = {}
-        for each_string in strs:
-            word = ''.join(sorted(each_string))
-            if word in Anagram_map:
-                Anagram_map[word].append(each_string)
-            else:
-                Anagram_map[word] = [each_string]
-        
-        return Anagram_map.values()
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
