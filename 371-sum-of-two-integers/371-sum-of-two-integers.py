7e51e90481dc7f20e80b1259283126ef8809a055
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-10-08 21:00:41 -0400

    Time: 35 ms (86.01%), Space: 13.8 MB (62.73%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.py
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -1,21 +1,8 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        x, y = abs(a), abs(b)
-        if x < y:
-            return self.getSum(b, a)
+        mask = 0xFFFFFFFF
+        while b!=0:
+            a, b = (a^b) & mask, ((a & b)<<1) & mask
         
-        sign = 1 if a > 0 else -1
-        
-        if a*b >= 0:
-            while y:
-                answer = x ^ y
-                carry = (x & y) << 1 
-                x, y = answer, carry
-        
-        else:
-            while y:
-                answer = x ^ y
-                borrow = ((~x) & y) << 1
-                x, y = answer, borrow
-        
-        return x * sign
+        max_int = 0x7FFFFFFF
+        return a if a < max_int else ~(a^mask)
