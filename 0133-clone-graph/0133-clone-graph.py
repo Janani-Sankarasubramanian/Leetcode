bb09cc8ba0adb6e0e9944ab450b41a7f2393678f
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-10-11 08:44:55 -0400

    Time: 78 ms (30.47%), Space: 14.5 MB (26.16%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -5,22 +5,23 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
+from collections import deque
 class Solution:
-    def __init__(self):
-        self.visited = {}
-        
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node:
             return node
         
-        if node in self.visited:
-            return self.visited[node]
+        visited = {}
+        queue = deque([node])
+        visited[node] = Node(node.val, [])
         
-        clone_node = Node(node.val, [])
-        self.visited[node] = clone_node
+        while queue:
+            n = queue.popleft()
+            for neighbor in n.neighbors:
+                if neighbor not in visited:
+                    visited[neighbor] = Node(neighbor.val, [])
+                    queue.append(neighbor)
         
-        if node.neighbors:
-            clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
-        
-        return clone_node
+                visited[n].neighbors.append(visited[neighbor])
+            
+        return visited[node]
