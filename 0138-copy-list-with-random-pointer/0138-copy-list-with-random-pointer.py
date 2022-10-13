3de215bf1d07f198c44890631384140d1bb4b4e8
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-10-13 08:15:57 -0400

    Time: 84 ms (8.26%), Space: 14.9 MB (82.75%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -8,29 +8,31 @@ class Node:
 """
 
 class Solution:
-    def __init__(self):
-        self.visitedHash = {}
-        
-    def getClonedNode(self, node):
-        if node:
-            if node in self.visitedHash:
-                return self.visitedHash[node]
-            else:
-                self.visitedHash[node] = Node(node.val, None, None)
-                return self.visitedHash[node]
-        return None
     
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return head
-        old_node = head
-        new_node = Node(old_node.val, None, None)
-        self.visitedHash[old_node] = new_node
         
-        while old_node != None:
-            new_node.random = self.getClonedNode(old_node.random)
-            new_node.next = self.getClonedNode(old_node.next)
-            old_node = old_node.next
-            new_node = new_node.next
+        ptr = head
+        while ptr:
+            new_node = Node(ptr.val, None, None)
+            new_node.next = ptr.next
+            ptr.next = new_node
+            ptr = new_node.next
+        
+        ptr = head
+        while ptr:
+            ptr.next.random = ptr.random.next if ptr.random else None
+            ptr = ptr.next.next
             
-        return self.visitedHash[head]
+        ptr_old_list = head
+        ptr_new_list = head.next
+        head_new = head.next
+        while ptr_old_list:
+            ptr_old_list.next = ptr_old_list.next.next
+            ptr_new_list.next = ptr_new_list.next.next if ptr_new_list.next else None
+            ptr_old_list = ptr_old_list.next
+            ptr_new_list = ptr_new_list.next
+        
+        return head_new
+            
