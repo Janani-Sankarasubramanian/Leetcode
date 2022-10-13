ed3da32a417cc1b1e0272d4a3e4f1a3420270e11
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-10-13 06:47:46 -0400

    Time: 48 ms (77.67%), Space: 15 MB (45.85%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -11,15 +11,26 @@ class Solution:
     def __init__(self):
         self.visitedHash = {}
         
+    def getClonedNode(self, node):
+        if node:
+            if node in self.visitedHash:
+                return self.visitedHash[node]
+            else:
+                self.visitedHash[node] = Node(node.val, None, None)
+                return self.visitedHash[node]
+        return None
+    
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if head == None:
-            return None
+        if not head:
+            return head
+        old_node = head
+        new_node = Node(old_node.val, None, None)
+        self.visitedHash[old_node] = new_node
         
-        if head in self.visitedHash:
-            return self.visitedHash[head]
-        
-        node = Node(head.val, None, None)
-        self.visitedHash[head] = node
-        node.next = self.copyRandomList(head.next)
-        node.random = self.copyRandomList(head.random)
-        return node
+        while old_node != None:
+            new_node.random = self.getClonedNode(old_node.random)
+            new_node.next = self.getClonedNode(old_node.next)
+            old_node = old_node.next
+            new_node = new_node.next
+            
+        return self.visitedHash[head]
