8e9a5105e80896949b9ffec1e4aa63b1013eadeb
Janani Sankarasubramanian <52078565+Janani-Sankarasubramanian@users.noreply.github.com>
2022-10-13 10:34:25 -0400

    Time: 67 ms (46.96%), Space: 14.9 MB (82.75%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -10,29 +10,19 @@ class Node:
 class Solution:
     
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return head
+        oldToCopy = {None: None}
         
-        ptr = head
-        while ptr:
-            new_node = Node(ptr.val, None, None)
-            new_node.next = ptr.next
-            ptr.next = new_node
-            ptr = new_node.next
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
+            cur = cur.next
         
-        ptr = head
-        while ptr:
-            ptr.next.random = ptr.random.next if ptr.random else None
-            ptr = ptr.next.next
+        cur = head
+        while cur:
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random = oldToCopy[cur.random]
+            cur = cur.next
             
-        ptr_old_list = head
-        ptr_new_list = head.next
-        head_new = head.next
-        while ptr_old_list:
-            ptr_old_list.next = ptr_old_list.next.next
-            ptr_new_list.next = ptr_new_list.next.next if ptr_new_list.next else None
-            ptr_old_list = ptr_old_list.next
-            ptr_new_list = ptr_new_list.next
-        
-        return head_new
-            
+        return oldToCopy[head]
